38c4cfabce0c7eff9b2f285e0d14084969b57155
Patrik Lojan <59892015+vetronn@users.noreply.github.com>
2025-12-01 09:23:52 +0000

    create main terrafrom file

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# 1. Stiahni Image (ako 'docker pull nginx')
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# 2. Spusti Kontajner (ako 'docker run ...')
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "moj-tofu-server"
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
